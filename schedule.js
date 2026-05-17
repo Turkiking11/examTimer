ec77beca73bba9982538717ac534d6a98ffb775a
Turkiking11 <136961973+Turkiking11@users.noreply.github.com>
2026-05-17 13:48:20 +0300

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,6 +1,6 @@
-const examSchedule = [
 
-    // GRADE 7
+let examSchedule = [
+
     {
         grade: "Grade 7",
         subject: "Mathematics",
@@ -9,47 +9,17 @@ const examSchedule = [
     },
 
     {
-        grade: "Grade 7",
-        subject: "Science",
-        start: "12:00",
-        end: "14:00"
-    },
-
-    {
-        grade: "Grade 7",
-        subject: "English",
-        start: "12:30",
-        end: "13:43"
-    },
-
-    // GRADE 8
-    {
         grade: "Grade 8",
-        subject: "Biology",
-        start: "08:30",
+        subject: "English",
+        start: "08:00",
         end: "10:00"
     },
 
     {
-        grade: "Grade 8",
+        grade: "Grade 9",
         subject: "Physics",
-        start: "10:30",
-        end: "12:30"
-    },
-
-    // GRADE 9
-    {
-        grade: "Grade 9",
-        subject: "Chemistry",
-        start: "09:00",
-        end: "11:00"
-    },
-
-    {
-        grade: "Grade 9",
-        subject: "Computer",
-        start: "11:30",
-        end: "13:30"
+        start: "08:00",
+        end: "10:00"
     }
 
 ];
