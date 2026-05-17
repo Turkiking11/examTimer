35038e0fd805a430a64472d7d1c8c8ff5d6532b6
Turkiking11 <136961973+Turkiking11@users.noreply.github.com>
2026-05-17 13:35:55 +0300

    Add files via upload

---
+++ schedule.js
@@ -0,0 +1,55 @@
+const examSchedule = [
+
+    // GRADE 7
+    {
+        grade: "Grade 7",
+        subject: "Mathematics",
+        start: "08:00",
+        end: "10:00"
+    },
+
+    {
+        grade: "Grade 7",
+        subject: "Science",
+        start: "10:30",
+        end: "12:00"
+    },
+
+    {
+        grade: "Grade 7",
+        subject: "English",
+        start: "12:30",
+        end: "14:00"
+    },
+
+    // GRADE 8
+    {
+        grade: "Grade 8",
+        subject: "Biology",
+        start: "08:30",
+        end: "10:00"
+    },
+
+    {
+        grade: "Grade 8",
+        subject: "Physics",
+        start: "10:30",
+        end: "12:30"
+    },
+
+    // GRADE 9
+    {
+        grade: "Grade 9",
+        subject: "Chemistry",
+        start: "09:00",
+        end: "11:00"
+    },
+
+    {
+        grade: "Grade 9",
+        subject: "Computer",
+        start: "11:30",
+        end: "13:30"
+    }
+
+];
